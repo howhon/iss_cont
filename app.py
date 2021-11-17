0c7a77a43cc8261b6a9788031a64b70998357835
Ho How Hon <howhon168@gmail.com>
2021-11-17 10:46:16 +0800

    Working
    - template uses static path to image
    - resize image
    - added handling of /index/

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import os
 from random import randint
 
-IMG_FOLDER = os.path.join('static', 'images')
 
 app = Flask(__name__)
 
@@ -14,11 +13,11 @@ quotes.append("It's not that I'm so smart, it's just that I stay with problems l
 quotes.append("It is pitch dark.  You are likely to be eaten by a grue.")
 
 @app.route('/')
+@app.route('/index/')
 def hello():
-    img_filename = os.path.join(IMG_FOLDER, 'image.gif')
     idx = randint(0,4)
     quote = quotes[idx]
-    return render_template('template.html', user_image=img_filename, display_text=quote)
+    return render_template('template.html', display_text=quote)
     
     
 if __name__ == '__main__':
